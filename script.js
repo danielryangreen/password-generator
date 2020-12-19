670de343871218f419116c70a39f5f9b87e25ed1
Dan Green <dan.ryan.green@gmail.com>
2020-12-18 19:56:56 -0700

    Entered special characters and 'for loop' for testing

--- script.js
+++ script.js
@@ -22,8 +22,18 @@ function generatePassword() {
   var charactersLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var charactersUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var charactersNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var charactersSpecial = [];
-  var charactersSelected = [];
+  var charactersSpecial = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+  var arrayEmpty = [];
+  var passwordString = "";
+
+  // add characters to selected array
+  // SPACE CHARACTER IS CAUSING A LINE BREAK???
+  var charactersSelected = arrayEmpty.concat(charactersLowercase, charactersUppercase, charactersNumeric, charactersSpecial);
+
+  // add characters to password string
+  for (var j = 0; j < charactersSelected.length; j++) {
+    passwordString = passwordString + charactersSelected[j];
+  }
   
-  return "abc123";
+  return passwordString;
 }
